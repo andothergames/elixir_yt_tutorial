d73fe799d841a3130d75fbe7441c49ce7a671e8e
Joy <32204909+andothergames@users.noreply.github.com>
2024-10-16 15:05:53 +0100

    update: guards documentation

--- lib/guards.ex
+++ lib/guards.ex
@@ -1,7 +1,10 @@
 defmodule Guards do
-  # guards expand on pattern matchingwith complex checks
-  # guards begin with the when keyword folllowed by a boolean expression
-  # these expressions are special pure functions that return the same value for a given input, true or false
+  @moduledoc """
+  Documentation for `Guards`.
+  Guards expand on pattern matchingwith complex checks
+  Guards begin with the when keyword folllowed by a boolean expression
+  These expressions are special pure functions that return the same value for a given input, true or false
+  """
 
   # a common guard is type checks, Kernel gives us functions to use for this
   def type(value) when is_integer(value), do: :integer
@@ -12,14 +15,12 @@ defmodule Guards do
   def is_single_digit(value) when is_integer(value) and value < 10, do: true
   def is_single_digit(value) when not is_integer(value) or value > 9, do: false
 
-  # define own guardsw ith defguard, defguardp (for private)
+  # define own guards with defguard, defguardp (for private)
   defguardp is_even(value) when is_integer(value) and rem(value, 2) == 0
 
   def return_even_numbers(value \\ :empty)
   def return_even_numbers(value) when is_even(value), do: value
   def return_even_numbers(value) when not is_even(value), do: :not_even
 
-  # challenges
-  # write tests and documentation for these functions
   # create the is_under_max_load? function from pattern matching using guards
 end
